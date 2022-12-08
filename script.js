a6d51ac77975fa976a49ee165ed56b571d85ba75
Jomanahani <jomanahani123456@gmail.com>
2022-12-08 07:57:15 +0200

    add main section

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 // access html element
-const Timer = document.querySelector(".timer")
-const Stopwatch = document.querySelector(".stopwatch")
-const main =document.querySelector(".main")
+const Timer = document.querySelector(".timer");
+const Stopwatch = document.querySelector(".stopwatch");
+const main = document.querySelector(".main");
+const timerSection = document.querySelector(".timerSec");
+const StopwatchSection = document.querySelector(".stopSec");
 
 // add event listeners
-Stopwatch.addEventListener("click",()=>{
-    Timer.classList.remove("active")
-    Stopwatch.classList.add("active")
-    createStopwatchElement();
-})
+Stopwatch.addEventListener("click", () => {
+  Timer.classList.remove("active");
+  Stopwatch.classList.add("active");
+  StopwatchSection.style.display = "block";
+  timerSection.style.display = "none";
+});
 
-Timer.addEventListener("click",()=>{
-    Stopwatch.classList.remove("active")
-    Timer.classList.add("active")
-    createTimerElement()
-})
-// functions to create elements for the selected type
-function createStopwatchElement(){
+Timer.addEventListener("click", () => {
+  Stopwatch.classList.remove("active");
+  Timer.classList.add("active");
+  StopwatchSection.style.display = "none";
+  timerSection.style.display = "block";
+});
 
-}
-function createTimerElement(){
-
-}
